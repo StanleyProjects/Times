2b0910ad80e7fcb98cfd86a875110613169271c9
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-21 16:11:38 +0300

    test times

--- lib/src/test/kotlin/sp/kx/times/RealTimesTest.kt
+++ lib/src/test/kotlin/sp/kx/times/RealTimesTest.kt
@@ -1,10 +1,20 @@
 package sp.kx.times
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class RealTimesTest {
     @Test
     fun nowTest() {
-        TODO("RealTimesTest:nowTest")
+        val times: Times = RealTimes()
+        val t0 = System.currentTimeMillis()
+        val t1 = times.now().inWholeMilliseconds
+        val t2 = System.currentTimeMillis()
+        val message = """
+            t0: $t0
+            t1: $t1
+            t2: $t2
+        """.trimIndent()
+        assertTrue(t1 in t0..t2, message)
     }
 }
